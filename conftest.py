3fedd10dd38113828af2563783fd876c1506ba74
EkaterinaMangileva <mangileva.ek@gmail.com>
2025-05-28 13:43:44 +0500

    пробные исправления для тестов

--- conftest.py
+++ conftest.py
@@ -18,23 +18,33 @@ def load_env():
     load_dotenv()
 
 
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    # Получаем результат выполнения теста
-    outcome = yield
-    result = outcome.get_result()
+@pytest.fixture(scope="function", autouse=True)
+def screenshot_failure(request, page):
+    yield
+    if request.node.rep_setup.failed:
+        (allure.attach(page.screenshot, name=f"screenshots/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png"))
+    elif request.node.rep_setup.passed:
+        if request.node.rep_call.failed:
+            (allure.attach(page.screenshot, name=f"screenshots/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png"))
 
-    # Скриншоты при падении или пропуске (failed или skipped)
-    if result.when == "call" and (result.failed or result.skipped):
-        page = item.funcargs.get("page", None)
-        if page:
-            try:
-                timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-                screenshot = page.screenshot()
-                allure.attach(
-                    screenshot,
-                    name=f"screenshot_{timestamp}",
-                    attachment_type=allure.attachment_type.PNG
-                )
-            except Exception as e:
-                print(f"Ошибка при создании скриншота: {e}")
+
+# @pytest.hookimpl(hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     # Получаем результат выполнения теста
+#     outcome = yield
+#     result = outcome.get_result()
+#
+#     # Скриншоты при падении или пропуске (failed или skipped)
+#     if result.when == "call" and (result.failed or result.skipped):
+#         page = item.funcargs.get("page", None)
+#         if page:
+#             try:
+#                 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+#                 screenshot = page.screenshot()
+#                 allure.attach(
+#                     screenshot,
+#                     name=f"screenshot_{timestamp}",
+#                     attachment_type=allure.attachment_type.PNG
+#                 )
+#             except Exception as e:
+#                 print(f"Ошибка при создании скриншота: {e}")
